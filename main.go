9be9a40e037eb70451d7818d821d85d9f9cb5156
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-12-06 20:32:43 -0500

    let's just parse these vcf files ourselves

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/mendelics/vcf"
+	"strings"
 )
+
+type Variant struct {
+	Chromosome  string
+	Position    string
+	Id          string
+	Reference   string
+	Alternative string
+	Qual        string
+	Filter      string
+	Info        string
+	Format      string
+	Additional  []string
+}
 
 func main() {
 
@@ -25,34 +37,57 @@ func main() {
 		return
 	}
 
-	vcfFile, err := os.Open(*filename)
+	f, err := os.Open(*filename)
 	if err != nil {
 		fmt.Println("Error: Can't open file", *filename, err)
 		return
 	}
-	defer vcfFile.Close()
+	defer f.Close()
 
-	validVariants := make(chan *vcf.Variant, 2000)     // buffered channel for correctly parsed variants
-	invalidVariants := make(chan vcf.InvalidLine, 100) // buffered channel for variants that fail to parse
+	var variants []Variant
 
-	go func() {
-		err := vcf.ToChannel(vcfFile, validVariants, invalidVariants)
-		if err != nil {
-			log.Fatalln(err)
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			continue
 		}
-	}()
-
-	go func() {
-		// consume invalid variants channel asynchronously
-		for invalid := range invalidVariants {
-			fmt.Println("failed to parse line", invalid.Line, "with error", invalid.Err)
+		fields := strings.Split(line, ";")
+		if len(fields) < 3 {
+			fmt.Println("Error: could not parse vcf file. Something is wrong here:", line)
+			return
 		}
-	}()
+		// grab the first part of each line (contains chromosome, ref, alt)
+		variantInfo := strings.Split(fields[0], "\t")
+		format := fields[1]
+		additionalFields := fields[2 : len(fields)-1]
 
-	for variant := range validVariants {
-		fmt.Println(variant)
+		chromosome := variantInfo[0]
+		pos := variantInfo[1]
+		id := variantInfo[2]
+		ref := variantInfo[3]
+		alt := variantInfo[4]
+		qual := variantInfo[5]
+		filter := variantInfo[6]
+		info := variantInfo[7]
+
+		variants = append(variants, Variant{
+			chromosome,
+			pos,
+			id,
+			ref,
+			alt,
+			qual,
+			filter,
+			info,
+			format,
+			additionalFields,
+		})
+
 	}
 
+	fmt.Println(variants)
 	u := "http://genetics.bwh.harvard.edu/cgi-bin/ggi/ggi2.cgi"
 	fmt.Println(u)
 }
